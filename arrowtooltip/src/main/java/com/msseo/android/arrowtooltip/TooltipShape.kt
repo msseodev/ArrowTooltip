0fe03c9f2809fbddd47aa115bd81f21050f12ff7
msseo <msseo1040@gmail.com>
2025-03-13 13:39:54 +0900

    [TooltipShape] Implement corner arrow.

--- arrowtooltip/src/main/java/com/msseo/android/arrowtooltip/TooltipShape.kt
+++ arrowtooltip/src/main/java/com/msseo/android/arrowtooltip/TooltipShape.kt
@@ -1,15 +1,30 @@
 package com.msseo.android.arrowtooltip
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.RoundRect
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import kotlin.random.Random
 
 /**
  * Data class representing the shape of the tooltip, including the corner radius, arrow size, and arrow position.
@@ -56,87 +71,255 @@ internal class ArrowTooltipShape(
             addRoundRect(RoundRect(rect, radius, radius))
 
             // Draw the arrow
-            val verticalArrowXPosition = if(arrowOffset <= 0) {
+            val verticalArrowXPosition = if (arrowOffset <= 0) {
                 size.width / 2
             } else {
                 arrowOffset.coerceIn(minimumValue = arrow, maximumValue = size.width - arrow)
             }
-            val horizontalArrowYPosition = if(arrowOffset <= 0) {
+            val horizontalArrowYPosition = if (arrowOffset <= 0) {
                 size.height / 2
             } else {
                 arrowOffset.coerceIn(minimumValue = arrow, maximumValue = size.height - arrow)
             }
 
+            val isArrowOnCorner = when (arrowPosition) {
+                TooltipShape.ArrowPosition.Top, TooltipShape.ArrowPosition.Bottom -> verticalArrowXPosition <= arrow || verticalArrowXPosition >= size.width - arrow
+                TooltipShape.ArrowPosition.Left, TooltipShape.ArrowPosition.Right -> horizontalArrowYPosition <= arrow || horizontalArrowYPosition >= size.height - arrow
+            }
+
             when (arrowPosition) {
                 TooltipShape.ArrowPosition.Top -> {
                     val arrowLeft = verticalArrowXPosition - arrow
-                    moveTo(arrowLeft, arrow)
-                    lineTo(verticalArrowXPosition, 0f)
-                    lineTo(verticalArrowXPosition + arrow, arrow)
-                    close()
-
-                    // Draw rectangle below the arrow to cover the rounded corner
-                    addRect(
-                        Rect(
-                            left = arrowLeft,
-                            top = arrow,
-                            right = arrowLeft + radius,
-                            bottom = arrow + radius
+                    if (isArrowOnCorner) {
+                        val bottomLeftPoint = if (verticalArrowXPosition <= arrow) {
+                            // Arrow is on left corner
+                            moveTo(0f, 0f)
+                            relativeLineTo(0f, arrow)
+                            relativeLineTo(arrow, 0f)
+                            close()
+                            Offset(0f, arrow)
+                        } else {
+                            // Arrow is on right corner
+                            moveTo(size.width, 0f)
+                            relativeLineTo(0f, arrow)
+                            relativeLineTo(-arrow, 0f)
+                            close()
+                            Offset(size.width - radius, arrow)
+                        }
+                        // Draw rectangle below the arrow to cover the rounded corner
+                        addRect(
+                            Rect(
+                                left = bottomLeftPoint.x,
+                                top = bottomLeftPoint.y,
+                                right = bottomLeftPoint.x + radius,
+                                bottom = bottomLeftPoint.y + radius
+                            )
                         )
-                    )
+                    } else {
+                        moveTo(arrowLeft, arrow)
+                        lineTo(verticalArrowXPosition, 0f)
+                        lineTo(verticalArrowXPosition + arrow, arrow)
+                        close()
+                    }
                 }
+
                 TooltipShape.ArrowPosition.Bottom -> {
                     val arrowLeft = verticalArrowXPosition - arrow
-                    moveTo(arrowLeft, size.height - arrow)
-                    lineTo(verticalArrowXPosition, size.height)
-                    lineTo(verticalArrowXPosition + arrow, size.height - arrow)
-                    close()
-
-                    // Draw rectangle below the arrow to cover the rounded corner
-                    addRect(
-                        Rect(
-                            left = arrowLeft,
-                            top = size.height - arrow - radius,
-                            right = arrowLeft + arrow + radius,
-                            bottom = size.height - arrow
+                    if (isArrowOnCorner) {
+                        val topLeftPoint = if (verticalArrowXPosition <= arrow) {
+                            // Arrow is on left corner
+                            moveTo(0f, size.height)
+                            relativeLineTo(0f, -arrow)
+                            relativeLineTo(arrow, 0f)
+                            close()
+                            Offset(0f, size.height - arrow - radius)
+                        } else {
+                            // Arrow is on right corner
+                            moveTo(size.width, size.height)
+                            relativeLineTo(0f, -arrow)
+                            relativeLineTo(-arrow, 0f)
+                            close()
+                            Offset(size.width - radius, size.height - arrow - radius)
+                        }
+                        // Draw rectangle below the arrow to cover the rounded corner
+                        addRect(
+                            Rect(
+                                left = topLeftPoint.x,
+                                top = topLeftPoint.y,
+                                right = topLeftPoint.x + radius,
+                                bottom = topLeftPoint.y + radius
+                            )
                         )
-                    )
+                    } else {
+                        moveTo(arrowLeft, size.height - arrow)
+                        lineTo(verticalArrowXPosition, size.height)
+                        lineTo(verticalArrowXPosition + arrow, size.height - arrow)
+                        close()
+                    }
                 }
 
                 TooltipShape.ArrowPosition.Left -> {
-                    moveTo(arrow, horizontalArrowYPosition - arrow)
-                    lineTo(0f, horizontalArrowYPosition)
-                    lineTo(arrow, horizontalArrowYPosition + arrow)
-                    close()
+                    if (isArrowOnCorner) {
+                        val rightTopCorner = if (horizontalArrowYPosition <= arrow) {
+                            // Arrow is on top corner
+                            moveTo(0f, 0f)
+                            relativeLineTo(arrow, 0f)
+                            relativeLineTo(0f, arrow)
+                            close()
+                            Offset(arrow, 0f)
+                        } else {
+                            // Arrow is on bottom corner
+                            moveTo(0f, size.height)
+                            relativeLineTo(arrow, 0f)
+                            relativeLineTo(0f, -arrow)
+                            close()
+                            Offset(arrow, size.height - radius)
+                        }
 
-                    // Draw rectangle below the arrow to cover the rounded corner
-                    addRect(
-                        Rect(
-                            left = arrow,
-                            top = horizontalArrowYPosition - arrow,
-                            right = arrow + radius,
-                            bottom = horizontalArrowYPosition + arrow
+                        // Draw rectangle below the arrow to cover the rounded corner
+                        addRect(
+                            Rect(
+                                left = rightTopCorner.x,
+                                top = rightTopCorner.y,
+                                right = rightTopCorner.x + radius,
+                                bottom = rightTopCorner.y + radius
+                            )
                         )
-                    )
+                    } else {
+                        moveTo(arrow, horizontalArrowYPosition - arrow)
+                        lineTo(0f, horizontalArrowYPosition)
+                        lineTo(arrow, horizontalArrowYPosition + arrow)
+                        close()
+                    }
                 }
 
                 TooltipShape.ArrowPosition.Right -> {
-                    moveTo(size.width - arrow, horizontalArrowYPosition - arrow)
-                    lineTo(size.width, horizontalArrowYPosition)
-                    lineTo(size.width - arrow, horizontalArrowYPosition + arrow)
-                    close()
+                    if (isArrowOnCorner) {
+                        val leftTopCorner = if (horizontalArrowYPosition <= arrow) {
+                            // Arrow is on top corner
+                            moveTo(size.width, 0f)
+                            relativeLineTo(-arrow, 0f)
+                            relativeLineTo(0f, arrow)
+                            close()
+                            Offset(size.width - arrow - radius, 0f)
+                        } else {
+                            // Arrow is on bottom corner
+                            moveTo(size.width, size.height)
+                            relativeLineTo(-arrow, 0f)
+                            relativeLineTo(0f, -arrow)
+                            close()
+                            Offset(size.width - arrow - radius, size.height - radius)
+                        }
 
-                    // Draw rectangle below the arrow to cover the rounded corner
-                    addRect(
-                        Rect(
-                            left = size.width - arrow - radius,
-                            top = horizontalArrowYPosition - arrow,
-                            right = size.width - arrow,
-                            bottom = horizontalArrowYPosition + arrow
+                        // Draw rectangle below the arrow to cover the rounded corner
+                        addRect(
+                            Rect(
+                                left = leftTopCorner.x,
+                                top = leftTopCorner.y,
+                                right = leftTopCorner.x + radius,
+                                bottom = leftTopCorner.y + radius
+                            )
                         )
-                    )
+                    } else {
+                        moveTo(size.width - arrow, horizontalArrowYPosition - arrow)
+                        lineTo(size.width, horizontalArrowYPosition)
+                        lineTo(size.width - arrow, horizontalArrowYPosition + arrow)
+                        close()
+                    }
                 }
             }
         }
     })
+}
+
+@Composable
+private fun ArrowBoxes(
+    modifier: Modifier = Modifier,
+    arrowPosition: TooltipShape.ArrowPosition,
+    text: String,
+) {
+    fun randomColor(): Color = Color(
+        red = Random.nextInt(128, 256),
+        green = Random.nextInt(128, 256),
+        blue = Random.nextInt(128, 256),
+    )
+
+    Row(modifier = modifier) {
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(
+                    color = randomColor(),
+                    shape = ArrowTooltipShape(
+                        cornerRadius = 20.dp,
+                        arrowPosition = arrowPosition,
+                        arrowOffset = 5000f
+                    )
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "$text right")
+        }
+
+        Box(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .size(100.dp)
+                .background(
+                    color = randomColor(),
+                    shape = ArrowTooltipShape(
+                        cornerRadius = 20.dp,
+                        arrowPosition = arrowPosition,
+                        arrowOffset = 1f
+                    )
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "$text left")
+        }
+
+        Box(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .size(100.dp)
+                .background(
+                    color = randomColor(),
+                    shape = ArrowTooltipShape(
+                        cornerRadius = 20.dp,
+                        arrowPosition = arrowPosition,
+                        arrowOffset = with(LocalDensity.current) { 50.dp.toPx() }
+                    )
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "$text center")
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewOffsetArrowTooltip() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        ArrowBoxes(
+            modifier = Modifier.padding(vertical = 8.dp),
+            arrowPosition = TooltipShape.ArrowPosition.Top,
+            text = "Top"
+        )
+        ArrowBoxes(
+            modifier = Modifier.padding(vertical = 8.dp),
+            arrowPosition = TooltipShape.ArrowPosition.Bottom,
+            text = "Bottom"
+        )
+        ArrowBoxes(
+            modifier = Modifier.padding(vertical = 8.dp),
+            arrowPosition = TooltipShape.ArrowPosition.Left,
+            text = "Left"
+        )
+        ArrowBoxes(
+            modifier = Modifier.padding(vertical = 8.dp),
+            arrowPosition = TooltipShape.ArrowPosition.Right,
+            text = "Right"
+        )
+    }
 }
