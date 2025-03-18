ade43d1a4554806404a52d97039410044be929dc
msseo <msseo1040@gmail.com>
2025-03-18 15:03:30 +0900

    [Build] Apply maven publish

--- arrowtooltip/build.gradle.kts
+++ arrowtooltip/build.gradle.kts
@@ -1,7 +1,11 @@
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.vanniktech.maven.publish)
 }
 
 android {
@@ -33,6 +37,51 @@ android {
     }
     buildFeatures {
         compose = true
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(
+        host = SonatypeHost.CENTRAL_PORTAL,
+        automaticRelease = true
+    )
+    signAllPublications()
+
+    configure(
+        AndroidSingleVariantLibrary(
+            variant = "release",
+            sourcesJar = true,
+            publishJavadocJar = true,
+        )
+    )
+
+    coordinates(
+        groupId = "io.github.msseodev",
+        artifactId = "arrowtooltip",
+        version = "0.0.1"
+    )
+    pom {
+        name.set("ArrowTooltip")
+        description.set("A modernized and sophisticated tooltips, fully customizable with an arrow for Android.")
+        inceptionYear.set("2025")
+        url.set("https://github.com/msseodev/ArrowTooltip")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("msseodev")
+                name.set("Myeongsoo Seo")
+                url.set("https://github.com/msseodev")
+            }
+        }
+        scm {
+            url.set("https://github.com/msseodev/ArrowTooltip")
+        }
     }
 }
 
